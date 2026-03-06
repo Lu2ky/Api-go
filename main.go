c6a1344510e20e2487189836f00b1d8c06678815
lu2ky <jaaa736504@gmail.com>
2026-03-06 02:56:06 +0000

    Metodo para añadir administrador de la web

--- main.go
+++ main.go
@@ -346,6 +346,7 @@ func main() {
 	//	LDAP
 	router.POST("/auth", auth)
 	router.POST("/addauthuser", createUser)
+	router.POST("/addadmin", createAdmin)
 
 	router.Run("0.0.0.0:8080") // The port number for expone the API
 	//router.Run(":8080")
@@ -2019,6 +2020,93 @@ func CreateLDAPUser(adminUser, adminPass, username, password string) error {
 	err = l.Modify(modGroup)
 	if err != nil {
 		return fmt.Errorf("error agregando al grupo Usuario: %v", err)
+	}
+
+	return nil
+}
+
+func createAdmin(c *gin.Context) {
+	err := CreateLDAPAdminUser(
+
+	)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Admin creado correctamente"})
+}	
+func CreateLDAPAdminUser(adminUser, adminPass, username, password string) error {
+	l, err := ldap.DialURL("ldap://" + os.Getenv("LDAP_ADDR") + ":" + os.Getenv("LDAP_PORT"))
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+
+	err = l.StartTLS(&tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		return err
+	}
+
+	err = l.Bind(adminUser+"@adhe.local", adminPass)
+	if err != nil {
+		return err
+	}
+
+	userDN := fmt.Sprintf("CN=%s,CN=Users,DC=adhe,DC=local", username)
+
+	addReq := ldap.NewAddRequest(userDN, nil)
+
+	addReq.Attribute("objectClass", []string{
+		"top",
+		"person",
+		"organizationalPerson",
+		"user",
+	})
+
+	addReq.Attribute("cn", []string{username})
+	addReq.Attribute("sAMAccountName", []string{username})
+	addReq.Attribute("userPrincipalName", []string{username + "@adhe.local"})
+	addReq.Attribute("displayName", []string{username})
+	addReq.Attribute("userAccountControl", []string{"544"})
+
+	err = l.Add(addReq)
+	if err != nil {
+		return err
+	}
+
+	quotedPwd := fmt.Sprintf("\"%s\"", password)
+	utf16Pwd := utf16.Encode([]rune(quotedPwd))
+
+	pwdBytes := make([]byte, len(utf16Pwd)*2)
+	for i, v := range utf16Pwd {
+		binary.LittleEndian.PutUint16(pwdBytes[i*2:], v)
+	}
+
+	modPwd := ldap.NewModifyRequest(userDN, nil)
+	modPwd.Replace("unicodePwd", []string{string(pwdBytes)})
+
+	err = l.Modify(modPwd)
+	if err != nil {
+		return fmt.Errorf("error seteando password: %v", err)
+	}
+	modEnable := ldap.NewModifyRequest(userDN, nil)
+	modEnable.Replace("userAccountControl", []string{"512"})
+
+	err = l.Modify(modEnable)
+	if err != nil {
+		return fmt.Errorf("error habilitando usuario: %v", err)
+	}
+
+	groupDN := fmt.Sprintf("CN=%s,CN=admin_upb_planner,DC=adhe,DC=local", username)
+
+	modGroup := ldap.NewModifyRequest(groupDN, nil)
+	modGroup.Add("member", []string{userDN})
+
+	err = l.Modify(modGroup)
+	if err != nil {
+		return fmt.Errorf("error agregando al grupo admin_upb_planner: %v", err)
 	}
 
 	return nil
