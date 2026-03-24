2daa43f861a22e8fce723248aa163c7211c4fdd5
Luky <160178490+Lu2ky@users.noreply.github.com>
2026-03-23 21:50:53 -0500

    Implement logging for comment operations
    Added logging for comment insert, update, and delete operations.

--- modulo_comment.go
+++ modulo_comment.go
@@ -105,6 +105,16 @@ func addPersonalComment(c *gin.Context) {
 		c.JSON(500, gin.H{"error": "Internal server error"})
 		return
 	}
+	insertedID, _ := result.LastInsertId()
+	
+
+	descripcion := "El id del comentario ingresado fue: " + strconv.FormatInt(insertedID, 10)
+
+	insertarLog(
+		newComment.N_idUsuario, 
+		"INSERTAR_COMENTARIO",
+		descripcion,
+	)
 
 	rowsAffected, _ := result.RowsAffected()
 	c.JSON(200, gin.H{
@@ -136,7 +146,13 @@ func updatePersonalComment(c *gin.Context) {
 		c.JSON(500, gin.H{"error": "Internal server error"})
 		return
 	}
+	descripcion := "Se actualizó el comentario con id: " + strconv.Itoa(newComment.N_idComentarios)
 
+	insertarLog(
+		newComment.N_idUsuario, 
+		"ACTUALIZAR_COMENTARIO",
+		descripcion,
+	)
 	rowsAffected, _ := result.RowsAffected()
 	c.JSON(200, gin.H{
 		"message":      "Comentario editado correctamente",
@@ -165,6 +181,12 @@ func deletePersonalComment(c *gin.Context) {
 		c.JSON(500, gin.H{"error": "Internal server error"})
 		return
 	}
+	
+	insertarLog(
+		delComment.N_idUsuario, 
+		"ELIMINAR_COMENTARIO",
+		descripcion,
+	)
 
 	rowsAffected, _ := result.RowsAffected()
 	c.JSON(200, gin.H{
