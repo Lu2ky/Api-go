09d0945b81e2d20fdf5e51cebbf3df20a08603e4
Nath-388 <nataliasofiarojaspimienta@gmail.com>
2026-04-01 22:15:23 -0500

    Update SQL query to select all columns
    Changed SQL query to select all columns from PeriodoAcademico.

--- modulo_official.go
+++ modulo_official.go
@@ -184,7 +184,7 @@ func getAcademicPeriods(c *gin.Context) {
 
 		El operador := lo que hace es definir una variable e inferir su tipo automáticamente.
 	*/
-	rows, err := db.Query(`SELECT N_idPeriodoAcademico, T_nombre, Dt_fechaInicio, Dt_fechaFinal FROM PeriodoAcademico;`)
+	rows, err := db.Query(`SELECT * FROM PeriodoAcademico;`)
 
 	//	si err != nil entonces significa que hay un error.
 	//	nil es similar a null. Entonces si el error es nulo significa que no hay errores.
